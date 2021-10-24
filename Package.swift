978b72b607b50cd1927ff3649b69166cc6e9877b
Yono Mittlefehldt <yono@toojuice.com>
2021-10-24 12:25:03 -0500

    Add Publish as a dependency

--- Package.swift
+++ Package.swift
@@ -6,21 +6,17 @@ import PackageDescription
 let package = Package(
     name: "VimeoPublishPlugin",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VimeoPublishPlugin",
             targets: ["VimeoPublishPlugin"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VimeoPublishPlugin",
-            dependencies: []),
+            dependencies: ["Publish"]),
         .testTarget(
             name: "VimeoPublishPluginTests",
             dependencies: ["VimeoPublishPlugin"]),
