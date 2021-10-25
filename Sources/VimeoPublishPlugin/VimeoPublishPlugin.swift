aa15a9efcbffefc15a02486fe3599f153f49b935
Yono Mittlefehldt <yono@toojuice.com>
2021-10-25 07:15:35 -0500

    Add the plugin logic

--- Sources/VimeoPublishPlugin/VimeoPublishPlugin.swift
+++ Sources/VimeoPublishPlugin/VimeoPublishPlugin.swift
@@ -1,6 +1,39 @@
-public struct VimeoPublishPlugin {
-    public private(set) var text = "Hello, World!"
+//
+//  VimeoPublishPlugin.swift
+//
+//
+//  Created by Yonatan Mittlefehldt on 2021-24-10.
+//
 
-    public init() {
+import Publish
+import Ink
+import Foundation
+
+public extension Plugin {
+    static func vimeo() -> Self {
+        Plugin(name: "Vimeo Video Embedder") { context in
+            context.markdownParser.addModifier(
+                .vimeoBlockquote()
+            )
+        }
+    }
+}
+
+public extension Modifier {
+    static func vimeoBlockquote() -> Self {
+        return Modifier(target: .blockquotes) { html, markdown in
+            let prefix = "vimeo "
+            var markdown = markdown.dropFirst().trimmingCharacters(in: .whitespaces)
+
+            guard markdown.hasPrefix(prefix) else {
+                return html
+            }
+
+            markdown = markdown.dropFirst(prefix.count).trimmingCharacters(in: .newlines)
+
+            let embeddedVideo = try! VimeoAPI.metadata(for: markdown)
+
+            return embeddedVideo.html
+        }
     }
 }
